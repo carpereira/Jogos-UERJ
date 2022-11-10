0ff16a781c2dc04a324ca62f36020030610581e0
carpereira <59072054+carpereira@users.noreply.github.com>
2022-11-10 19:07:08 -0300

    Update Exercício1.2.2.c

--- Exercício1.2.2.c
+++ Exercício1.2.2.c
@@ -1,11 +1,20 @@
 #define SDL2_GFXPRIMITIVES_SCOPE
+#define _SDL2_gfxPrimitives_h
+#define M_PI    3.1415926535897932384626433832795
+#define SDL2_GFXPRIMITIVES_MAJOR        1
+#define SDL2_GFXPRIMITIVES_MINOR        0
+#define SDL2_GFXPRIMITIVES_MICRO        1
+#define SDL2_GFXPRIMITIVES_SCOPE __declspec(dllexport)
+#define SDL2_GFXPRIMITIVES_SCOPE __declspec(dllimport)
+#define SDL2_GFXPRIMITIVES_SCOPE extern
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
-//#include <SDL_rotozoom.h>
+#include <SDL/SDL2>
+#include <SDL_rotozoom.h>
 #include <SDL2_gfxPrimitives.h>
 #include <SDL2_gfxPrimitives_font.h>
 
@@ -20,11 +29,36 @@ SDL_Window* win = SDL_CreateWindow("Figuras Complexas!",
                       );
 SDL_Renderer* ren = SDL_CreateRenderer(win, -1,0);
 
-/*EXECUÇÃO*/
-SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-SDL_RenderClear(ren);
-/*circulo*/  
-SDL2_GFXPRIMITIVES_SCOPE int aacircleColor(SDL_Renderer * renderer,200,200,30,0xFF);
+/*EXECUÇÂO*/
+ 
+/* Note: all ___Color routines expect the color to be in format 0xRRGGBBAA */
+ 
+/* Pixel */
+ 
+SDL2_GFXPRIMITIVES_SCOPE int pixelColor(SDL_Renderer * renderer, Sint16 x, Sint16 y, Uint32 color);
+SDL2_GFXPRIMITIVES_SCOPE int pixelRGBA(SDL_Renderer * renderer, Sint16 x, Sint16 y, Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+ 
+/* Horizontal line */
+ 
+SDL2_GFXPRIMITIVES_SCOPE int hlineColor(SDL_Renderer * renderer, Sint16 x1, Sint16 x2, Sint16 y, Uint32 color);
+SDL2_GFXPRIMITIVES_SCOPE int hlineRGBA(SDL_Renderer * renderer, Sint16 x1, Sint16 x2, Sint16 y, Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+ 
+/* Vertical line */
+ 
+SDL2_GFXPRIMITIVES_SCOPE int vlineColor(SDL_Renderer * renderer, Sint16 x, Sint16 y1, Sint16 y2, Uint32 color);
+SDL2_GFXPRIMITIVES_SCOPE int vlineRGBA(SDL_Renderer * renderer, Sint16 x, Sint16 y1, Sint16 y2, Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+ 
+/* Rectangle */
+ 
+SDL2_GFXPRIMITIVES_SCOPE int rectangleColor(SDL_Renderer * renderer, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, Uint32 color);
+SDL2_GFXPRIMITIVES_SCOPE int rectangleRGBA(SDL_Renderer * renderer, Sint16 x1, Sint16 y1,
+     Sint16 x2, Sint16 y2, Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+ 
+/* Rounded-Corner Rectangle */
+ 
+SDL2_GFXPRIMITIVES_SCOPE int roundedRectangleColor(SDL_Renderer * renderer, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, Sint16 rad, Uint32 color);
+SDL2_GFXPRIMITIVES_SCOPE int roundedRectangleRGBA(SDL_Renderer * renderer, Sint16 x1, Sint16 y1,
+Sint16 x2, Sint16 y2, Sint16 rad, Uint8 r, Uint8 g, Uint8 b, Uint8 a);
    
   
 SDL_RenderPresent(ren);
