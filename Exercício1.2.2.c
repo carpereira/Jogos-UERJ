2bdc4b28dd6c23e999ac3f733f9b9f60696f5d47
carpereira <59072054+carpereira@users.noreply.github.com>
2022-11-10 23:04:33 -0300

    Update Exercício1.2.2.c

--- Exercício1.2.2.c
+++ Exercício1.2.2.c
@@ -55,7 +55,11 @@ SDL_RenderClear(ren);
  
 /* Pixel */
  
-SDL2_GFXPRIMITIVES_SCOPE int pixelColor(SDL_Renderer * renderer, Sint16 200, Sint16 200, Uint32 255);
+SDL2_GFXPRIMITIVES_SCOPE int pixelColor(SDL_Renderer * renderer, Sint16 200, Sint16 200, Uint32 255)
+{
+	Uint8 *c = (Uint8 *)&color;
+	return pixelRGBA(renderer,200,200, c[0],c[1],c[2],c[3]);
+}
 SDL2_GFXPRIMITIVES_SCOPE int pixelRGBA(SDL_Renderer * renderer, Sint16 200, Sint16 200, Uint8 255, Uint8 255, Uint8 255, Uint8 0);
  
 /* Horizontal line */
