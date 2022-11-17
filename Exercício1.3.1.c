3f24baf85d5d868e61a00cc78b868efd6c261d53
carpereira <59072054+carpereira@users.noreply.github.com>
2022-11-17 13:43:16 -0300

    Update Exercício1.3.1.c

--- Exercício1.3.1.c
+++ Exercício1.3.1.c
@@ -14,25 +14,6 @@ SDL_Renderer* ren = SDL_CreateRenderer(win, -1,0);
 /*EXECUÇÃO*/
 
 SDL_Rect r={40,20,10,10};
-  
-  /*while (r.y>20){
-    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-    SDL_RenderClear(ren);
-    SDL_SetRenderDrawColor(ren, 0xFF,0x00,0xFF,0x00);
-    SDL_RenderFillRect(ren,&r);      
-    SDL_RenderPresent(ren);
-    SDL_Delay(300);
-    r.y -=2;*/
-    
-  while (r.y<280 && r.x>20){
-    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-    SDL_RenderClear(ren);
-    SDL_SetRenderDrawColor(ren, 0xFF,0x00,0x00,0x00);
-    SDL_RenderFillRect(ren,&r);      
-    SDL_RenderPresent(ren);
-    SDL_Delay(300);
-    r.x -=2;
-
       
   while (r.y<280){
     SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
@@ -50,12 +31,34 @@ SDL_Rect r={40,20,10,10};
     SDL_RenderFillRect(ren,&r);      
     SDL_RenderPresent(ren);
     SDL_Delay(300);
-    r.x +=2;   
-    
+    r.x +=2;    
   
-  }
   }  
-  }    
+  }  
+    
+SDL_Rect r={380,380,10,10};
+    
+   while (r.y>20){
+    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+    SDL_RenderClear(ren);
+    SDL_SetRenderDrawColor(ren, 0xFF,0x00,0xFF,0x00);
+    SDL_RenderFillRect(ren,&r);      
+    SDL_RenderPresent(ren);
+    SDL_Delay(300);
+    r.y -=2;
+      
+  while (r.x>20){
+    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+    SDL_RenderClear(ren);
+    SDL_SetRenderDrawColor(ren, 0xFF,0x00,0x00,0x00);
+    SDL_RenderFillRect(ren,&r);      
+    SDL_RenderPresent(ren);
+    SDL_Delay(300);
+    r.x -=2;
+  }
+    }
+  
+  
 
 /*FINALIZAÇÃO*/
 SDL_DestroyRenderer(ren);
