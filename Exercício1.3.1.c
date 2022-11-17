40a0c9dc4b3a65720e6acaf4263c2912eb6c0ade
carpereira <59072054+carpereira@users.noreply.github.com>
2022-11-17 10:36:50 -0300

    Update Exercício1.3.1.c

--- Exercício1.3.1.c
+++ Exercício1.3.1.c
@@ -14,24 +14,7 @@ SDL_Renderer* ren = SDL_CreateRenderer(win, -1,0);
 /*EXECUÇÃO*/
 
 SDL_Rect r={40,20,10,10};
-  while (r.x>20){
-    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-    SDL_RenderClear(ren);
-    SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
-    SDL_RenderFillRect(ren,&r);      
-    SDL_RenderPresent(ren);
-    SDL_Delay(300);
-    r.x -=2;
-    
-  while (r.y>20){
-    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-    SDL_RenderClear(ren);
-    SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
-    SDL_RenderFillRect(ren,&r);      
-    SDL_RenderPresent(ren);
-    SDL_Delay(300);
-    r.y -=2;
-    
+      
   while (r.x<280){
     SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
     SDL_RenderClear(ren);
@@ -49,6 +32,24 @@ SDL_Rect r={40,20,10,10};
     SDL_RenderPresent(ren);
     SDL_Delay(300);
     r.y +=2;   
+    
+  while (r.x>20){
+    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+    SDL_RenderClear(ren);
+    SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+    SDL_RenderFillRect(ren,&r);      
+    SDL_RenderPresent(ren);
+    SDL_Delay(300);
+    r.x -=2;
+    
+  while (r.y>20){
+    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+    SDL_RenderClear(ren);
+    SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+    SDL_RenderFillRect(ren,&r);      
+    SDL_RenderPresent(ren);
+    SDL_Delay(300);
+    r.y -=2;
  
   }  
   }  
