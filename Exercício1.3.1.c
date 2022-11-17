e94964bb7ef16ecaee244ae91c2a76025116e2c8
carpereira <59072054+carpereira@users.noreply.github.com>
2022-11-17 09:24:02 -0300

    Update Exercício1.3.1.c

--- Exercício1.3.1.c
+++ Exercício1.3.1.c
@@ -7,14 +7,14 @@ SDL_Init(SDL_INIT_EVERYTHING);
 SDL_Window* win = SDL_CreateWindow("Animação Simples!",
                       SDL_WINDOWPOS_UNDEFINED,
                       SDL_WINDOWPOS_UNDEFINED,
-                      400, 400, SDL_WINDOW_SHOWN
+                      300, 300, SDL_WINDOW_SHOWN
                       );
 SDL_Renderer* ren = SDL_CreateRenderer(win, -1,0);
 
 /*EXECUÇÃO*/
 
 SDL_Rect r={40,20,10,10};
-  while (r.x<100){
+  while (r.x<290){
     SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
     SDL_RenderClear(ren);
     SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
