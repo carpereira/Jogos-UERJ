06d1eed5800dff51609d0667b03b21839542ca74
carpereira <59072054+carpereira@users.noreply.github.com>
2022-11-17 10:27:46 -0300

    Update Exercício1.3.1.c

--- Exercício1.3.1.c
+++ Exercício1.3.1.c
@@ -33,6 +33,28 @@ SDL_Rect r={40,20,10,10};
    SDL_RenderPresent(ren);
    SDL_Delay(300);
     r.y +=2;
+    
+ while (r.x>0){
+    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+    SDL_RenderClear(ren);
+    SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+    SDL_RenderFillRect(ren,&r);
+      
+   SDL_RenderPresent(ren);
+   SDL_Delay(300);
+    r.x -=2;
+    
+  while (r.y>0){
+    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+    SDL_RenderClear(ren);
+    SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+    SDL_RenderFillRect(ren,&r);
+      
+   SDL_RenderPresent(ren);
+   SDL_Delay(300);
+    r.y -=2;
+  }  
+  }  
   }  
   }    
 
