376498ca2b94a05ee059c63f7f5c6e0564d1e6c8
carpereira <59072054+carpereira@users.noreply.github.com>
2022-11-17 21:53:03 -0300

    Update Exercício1.2.2.c

--- Exercício1.2.2.c
+++ Exercício1.2.2.c
@@ -1,73 +1,29 @@
-#ifndef _SDL2_gfxPrimitives_h
-#define _SDL2_gfxPrimitives_h
-#endif
-
-#include <math.h>
-#ifndef M_PI
-#define M_PI	3.1415926535897932384626433832795
-#endif
-
 #include <SDL2/SDL.h>
-
-/* Set up for C function definitions, even when using C++ */
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-	/* ----- Versioning */
-
-#define SDL2_GFXPRIMITIVES_MAJOR	1
-#define SDL2_GFXPRIMITIVES_MINOR	0
-#define SDL2_GFXPRIMITIVES_MICRO	2
-
-
-	/* ---- Function Prototypes */
-
-#ifdef _MSC_VER
-#  if defined(DLL_EXPORT) && !defined(LIBSDL2_GFX_DLL_IMPORT)
-#    define SDL2_GFXPRIMITIVES_SCOPE __declspec(dllexport)
-#  else
-#    ifdef LIBSDL2_GFX_DLL_IMPORT
-#      define SDL2_GFXPRIMITIVES_SCOPE __declspec(dllimport)
-#    endif
-#  endif
-#endif
-#ifndef SDL2_GFXPRIMITIVES_SCOPE
-#  define SDL2_GFXPRIMITIVES_SCOPE extern
-#endif
+#include <SDL2_gfxPrimitives.h>
 
 int main(int argc, char* args[])
 {
 /*INICIALIZAÇÃO*/
 SDL_Init(SDL_INIT_EVERYTHING);
-SDL_Window* win = SDL_CreateWindow("Figuras Complexas!",
+SDL_Window* win = SDL_CreateWindow("Linhas pontos e retangulos!",
                       SDL_WINDOWPOS_UNDEFINED,
                       SDL_WINDOWPOS_UNDEFINED,
-                      500, 500, SDL_WINDOW_SHOWN
+                      400, 400, SDL_WINDOW_SHOWN
                       );
 SDL_Renderer* ren = SDL_CreateRenderer(win, -1,0);
-SDL_Surface*s;	
 
-
-
-/*EXECUÇÂO*/
- 
-/* Note: all ___Color routines expect the color to be in format 0xRRGGBBAA */
-SDL_SetRenderDrawColor(ren, 0xFF, 0xFF, 0xFF, 0x00);
+/*EXECUÇÃO*/
+SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
 SDL_RenderClear(ren);
- 
-
-s=SDL_CreateRGBSurface(0,300,200,32,30,60,40,0);
-SDL_FillRect(s,NULL,SDL_MapRGB(s->format,0xFF,0x00,0x00));
-
-   
-  
+	
+/*retangulo*/  
+SDL2_GFXPRIMITIVES_SCOPE int rectangleColor(SDL_Renderer * renderer, Sint16 100, Sint16 100,Sint16 200, Sint16 200, Uint32 255);
+SDL2_GFXPRIMITIVES_SCOPE int rectangleRGBA(SDL_Renderer * renderer, Sint16 100, Sint16 100,Sint16 200, Sint16 200, Uint8 255, Uint8 0, Uint8 0, Uint8 0);
 SDL_RenderPresent(ren);
-SDL_Delay(9000);
+SDL_Delay(500);
 
 /*FINALIZAÇÃO*/
 SDL_DestroyRenderer(ren);
-SDL_FreeSurface(s);
 SDL_DestroyWindow(win);
 SDL_Quit();
 }
