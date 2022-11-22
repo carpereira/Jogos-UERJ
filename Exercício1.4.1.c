bd48aea5cc1240ca834a657b7312242b033c38ed
carpereira <59072054+carpereira@users.noreply.github.com>
2022-11-22 12:26:42 -0300

    Update Exercício1.4.1.c

--- Exercício1.4.1.c
+++ Exercício1.4.1.c
@@ -35,20 +35,20 @@ int main (int argc, char* args[])
             SDL_RenderFillRect(ren, &r);
             SDL_RenderPresent(ren);
             
-        if (evt.type == SDL_KEYDOWN) {
-            switch (evt.key.keysym.sym) {
-                case SDLK_UP:
-                    r.y -= 5;
-                    break;
-                case SDLK_DOWN:
-                    r.y += 5;
-                    break;
-                case SDLK_LEFT:
-                    r.x -= 5;
-                    break;
-                case SDLK_RIGHT:
-                    r.x += 5;
-                    break;
+     if (evt.type == SDL_KEYDOWN) {
+         switch (evt.key.keysym.sym) {
+                 case SDLK_UP:
+                 r.y -= 5;
+                 break;
+                 case SDLK_DOWN:
+                 r.y += 5;
+                 break;
+                 case SDLK_LEFT:
+                 r.x -= 5;
+                 break;
+                 case SDLK_RIGHT:
+                 r.x += 5;
+                 break;
                     
             }
         }
