f7f94cb9b40bea8ab698fcaeb14581f58d77df4d
carpereira <59072054+carpereira@users.noreply.github.com>
2022-11-22 12:08:45 -0300

    Update Exercício1.4.1.c

--- Exercício1.4.1.c
+++ Exercício1.4.1.c
@@ -59,13 +59,4 @@ int main (int argc, char* args[])
     SDL_DestroyWindow(win);
     SDL_Quit();
 }
-            }
-        }
-    }
-        }
-
-    /* FINALIZACAO */
-    SDL_DestroyRenderer(ren);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
-}
+     
