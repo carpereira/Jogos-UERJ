42e6fec0b188d7aa02b82670a1f454515f17ed91
carpereira <59072054+carpereira@users.noreply.github.com>
2022-11-25 22:15:16 -0300

    Update Exercício1.4.1.c

--- Exercício1.4.1.c
+++ Exercício1.4.1.c
@@ -29,20 +29,20 @@ int main (int argc, char* args[])
             break;
         }
         
-        int b;
-        for(b=255; b>0; b-50){
+        //int b;
+        //for(b=255; b>0; b-50){
         
         if (evt.type == SDL_MOUSEBUTTONDOWN){
             SDL_SetRenderDrawColor(ren, 255,255,255,0);
             SDL_RenderClear(ren);
             
-            SDL_SetRenderDrawColor(ren,255,b,0,0);
+            //SDL_SetRenderDrawColor(ren,255,b,0,0);
             
-            //SDL_SetRenderDrawColor(ren, 255,0,0,0);
+            SDL_SetRenderDrawColor(ren, 255,0,0,0);
             
             SDL_RenderFillRect(ren, &r);
             SDL_RenderPresent(ren);
-            SDL_Delay(500);}
+            SDL_Delay(500);//}
         }
         
         if (evt.type == SDL_KEYDOWN) {            
