64f3dd1f757f89100e63d77ebb3c0ea0c4419d4e
carpereira <59072054+carpereira@users.noreply.github.com>
2022-11-26 21:06:30 -0300

    Update Exercício1.5.1.c

--- Exercício1.5.1.c
+++ Exercício1.5.1.c
@@ -34,7 +34,9 @@ int main (int argc, char* args[])
         
         int isevt = SDL_WaitEventTimeout(&evt,500);
         if(isevt){
-            if(evt.type == SDL_MOUSEBUTTONDOWN);
+            if(evt.type == SDL_MOUSEBUTTONDOWN){
+                s.relx;
+                s.rely;                   
         }
         
         SDL_WaitEvent(&evt);        
