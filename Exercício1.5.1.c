0f2cfbd9aeece9b21ba624e36c28276e0e64353b
carpereira <59072054+carpereira@users.noreply.github.com>
2022-11-29 17:23:42 -0300

    Update Exercício1.5.1.c

--- Exercício1.5.1.c
+++ Exercício1.5.1.c
@@ -50,8 +50,6 @@ int main (int argc, char* args[])
         
                      
         if (evt.type == SDL_MOUSEMOTION){
-            int evt.motion.x;
-            int evt.motion.y;
             s.x=evt.motion.x, s.y=evt.motion.y;          
         }
                 
