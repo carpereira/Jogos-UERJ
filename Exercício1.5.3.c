36d6f7a4c2e55bdafffc38fa70bf7756378609c3
carpereira <59072054+carpereira@users.noreply.github.com>
2022-11-30 23:43:26 -0300

    Update Exercício1.5.3.c

--- Exercício1.5.3.c
+++ Exercício1.5.3.c
@@ -14,7 +14,7 @@ SDL_Renderer* ren = SDL_CreateRenderer(win, -1,0);
 /*EXECUÇÃO*/
 
 SDL_Rect r={40,20,10,10};
-SDL_Rect s=(100,20,150,60};
+SDL_Rect s={100,20,150,60};
   
   
     while (1) {        
