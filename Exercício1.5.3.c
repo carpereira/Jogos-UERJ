1a399b4e2fc57cfdbe3e9abd0922afca4abb8c27
carpereira <59072054+carpereira@users.noreply.github.com>
2022-11-30 23:35:32 -0300

    Update Exercício1.5.3.c

--- Exercício1.5.3.c
+++ Exercício1.5.3.c
@@ -14,6 +14,15 @@ SDL_Renderer* ren = SDL_CreateRenderer(win, -1,0);
 /*EXECUÇÃO*/
 
 SDL_Rect r={40,20,10,10};
+  
+  
+    while (1) {        
+        SDL_SetRenderDrawColor(ren, 255,255,255,0);
+        SDL_RenderClear(ren);
+        SDL_SetRenderDrawColor(ren, 0,0,255,0);
+        SDL_RenderFillRect(ren, &r);       
+        SDL_RenderPresent(ren);
+        
       
   SDL_Event evt;
   
