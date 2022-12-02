577c0fe4f847b45ec479c6a3e7e7e1c1fce5eaa3
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-01 22:22:52 -0300

    Update Exercício1.4.1.c

--- Exercício1.4.1.c
+++ Exercício1.4.1.c
@@ -40,7 +40,7 @@ int main (int argc, char* args[])
             //break;
         }        
                   
-        if (evt.type == SDL_MOUSEBUTTONDOWN){
+        else (evt.type == SDL_MOUSEBUTTONDOWN){
             SDL_SetRenderDrawColor(ren, 255,255,255,0);              
             SDL_RenderClear(ren);            
             SDL_SetRenderDrawColor(ren, 0,0,255,0);            
