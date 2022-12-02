f36a8da4c27e7c78c61c5d422452c2ba91731b9d
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-02 00:33:31 -0300

    Update Exercício1.4.1.c

--- Exercício1.4.1.c
+++ Exercício1.4.1.c
@@ -15,7 +15,7 @@ int main (int argc, char* args[])
 
     /* EXECUÇÃO */
     SDL_Rect r = { 100,100, 10,10 };
-    SDL_Rect s = { 100,100, 10,10 };
+    //SDL_Rect s = { 100,100, 10,10 };
     
     
     SDL_Event evt;
@@ -24,7 +24,7 @@ int main (int argc, char* args[])
         SDL_RenderClear(ren);
         SDL_SetRenderDrawColor(ren, 0,0,255,0);
         SDL_RenderFillRect(ren, &r);
-        SDL_RenderFillRect(ren, &s);
+        //SDL_RenderFillRect(ren, &s);
         SDL_RenderPresent(ren);
         
         SDL_WaitEvent(&evt);        
@@ -37,9 +37,9 @@ int main (int argc, char* args[])
             SDL_SetRenderDrawColor(ren, 255,255,255,0);
             SDL_RenderClear(ren);
             SDL_SetRenderDrawColor(ren, 255,0,0,0);
-            SDL_RenderFillRect(ren, &r);
+            SDL_RenderFillRect(ren, &s);
             SDL_RenderPresent(ren);
-            r.x=evt.motion.x, r.y=evt.motion.y;
+            s.x=evt.motion.x, s.y=evt.motion.y;
             //SDL_Delay(300);
             //break;
         }
