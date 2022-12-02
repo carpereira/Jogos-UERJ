2f441cdfcc6d52b8a8c895750c6b3656fbb11bbb
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-02 17:31:00 -0300

    Update Exercício1.4.1.c

--- Exercício1.4.1.c
+++ Exercício1.4.1.c
@@ -40,7 +40,7 @@ int main (int argc, char* args[])
             SDL_SetRenderDrawColor(ren, 255,255,255,0);
             SDL_RenderClear(ren);
             SDL_SetRenderDrawColor(ren, 255,0,0,0);
-            SDL_Rect r = { x,y, 20,20 };
+            SDL_Rect r = { r.x,r.y, 20,20 };
             SDL_RenderFillRect(ren, &r);
             SDL_RenderPresent(ren);            
             //r.x, r.y;
@@ -56,7 +56,7 @@ int main (int argc, char* args[])
             SDL_SetRenderDrawColor(ren, 255,255,255,0);              
             SDL_RenderClear(ren);            
             SDL_SetRenderDrawColor(ren, 0,0,255,0);
-            SDL_Rect r = { x,y, 20,20 };
+            SDL_Rect r = { r.x,r.y, 20,20 };
             SDL_RenderFillRect(ren, &r);            
             SDL_RenderPresent(ren);            
             //r.x, r.y;
