afbb8d4826ad8340921c54aca1accf77d568d935
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-02 01:06:40 -0300

    Update Exercício1.5.3.c

--- Exercício1.5.3.c
+++ Exercício1.5.3.c
@@ -14,7 +14,7 @@ SDL_Renderer* ren = SDL_CreateRenderer(win, -1,0);
 /*EXECUÇÃO*/
 
 SDL_Rect r={40,20,10,10};
-SDL_Rect s={100,20,150,60};
+SDL_Rect s={280,280,150,60};
   
   SDL_Event evt;
     while (1) {        
@@ -53,20 +53,20 @@ SDL_Rect s={100,20,150,60};
       SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
       SDL_RenderClear(ren);
       SDL_SetRenderDrawColor(ren, 0x00,0xFF,0xFF,0x00);
-      SDL_RenderFillRect(ren,&r);      
+      SDL_RenderFillRect(ren,&s);      
       SDL_RenderPresent(ren);
       //SDL_Delay(100);
-      r.y -=1;
+      s.y -=1;
       break;}
       case 4:
         if (r.x>20){
       SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
       SDL_RenderClear(ren);
       SDL_SetRenderDrawColor(ren, 0xFF,0x00,0x00,0x00);
-      SDL_RenderFillRect(ren,&r);      
+      SDL_RenderFillRect(ren,&s);      
       SDL_RenderPresent(ren);
       //SDL_Delay(100);
-      r.x -=1;
+      s.x -=1;
       break;}
   
     }          
