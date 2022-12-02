dfe38779c5857a5dfb1980544e1c03e5bd6a7973
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-02 20:43:58 -0300

    Update Exercício1.4.1.c

--- Exercício1.4.1.c
+++ Exercício1.4.1.c
@@ -34,9 +34,7 @@ int main (int argc, char* args[])
 
             
         if (evt.type == SDL_MOUSEBUTTONDOWN){
-            r.x=evt.motion.x, r.y=evt.motion.y;
-        }
-        if (evt.type == SDL_MOUSEBUTTONUP){
+            r.x=evt.motion.x, r.y=evt.motion.y;   
             SDL_SetRenderDrawColor(ren, 255,255,255,0);
             SDL_RenderClear(ren);
             SDL_SetRenderDrawColor(ren, 255,0,0,0);
@@ -50,9 +48,7 @@ int main (int argc, char* args[])
      
                   
        if (evt.type == SDL_MOUSEBUTTONDOWN){
-            r.x=evt.motion.x, r.y=evt.motion.y;
-       }
-        if (evt.type == SDL_MOUSEBUTTONUP){
+            r.x=evt.motion.x, r.y=evt.motion.y;      
             SDL_SetRenderDrawColor(ren, 255,255,255,0);              
             SDL_RenderClear(ren);            
             SDL_SetRenderDrawColor(ren, 0,0,255,0);
