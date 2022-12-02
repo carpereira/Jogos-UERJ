ffb9da808681db0b71000e81154f25c044355b49
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-02 16:20:36 -0300

    Update Exercício1.4.1.c

--- Exercício1.4.1.c
+++ Exercício1.4.1.c
@@ -15,7 +15,7 @@ int main (int argc, char* args[])
 
     /* EXECUÇÃO */
     SDL_Rect r = { 100,100, 20,20 };
-    //SDL_Rect s = { 100,100, 10,10 };
+    SDL_Rect s = { 100,100, 10,10 };
     
     
     SDL_Event evt;
