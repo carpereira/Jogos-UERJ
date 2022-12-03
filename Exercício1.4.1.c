a2e8ed7aa82fcc2a1731dd60a7b6075b67bda496
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-02 22:49:56 -0300

    Update Exercício1.4.1.c

--- Exercício1.4.1.c
+++ Exercício1.4.1.c
@@ -51,7 +51,8 @@ int main (int argc, char* args[])
         if (evt.type == SDL_MOUSEBUTTONDOWN){
             s.x=evt.motion.x, s.y=evt.motion.y;      
             SDL_SetRenderDrawColor(ren, 255,255,255,0);              
-            SDL_RenderClear(ren);            
+            SDL_RenderClear(ren); 
+            SDL_Rect s = { s.x,s.y, 20,20 };
             SDL_SetRenderDrawColor(ren, 0,255,0,0);
             //SDL_Rect r = { r.x,r.y, 20,20 };
             SDL_RenderFillRect(ren, &s);            
