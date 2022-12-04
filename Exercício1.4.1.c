cd60dad5f684448cbf4ee6aa4bec90ff69799b8d
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-04 18:20:47 -0300

    Update Exercício1.4.1.c

--- Exercício1.4.1.c
+++ Exercício1.4.1.c
@@ -22,8 +22,8 @@ int main (int argc, char* args[])
     while (1) {        
         SDL_SetRenderDrawColor(ren, 255,255,255,0);
         SDL_RenderClear(ren);
-        SDL_SetRenderDrawColor(ren, 255,0,0,0);
-        SDL_RenderFillRect(ren, &r);
+        //SDL_SetRenderDrawColor(ren, 255,0,0,0);
+        //SDL_RenderFillRect(ren, &r);
         //SDL_SetRenderDrawColor(ren, 0,0,255,0);
         //SDL_RenderFillRect(ren, &s);
         SDL_RenderPresent(ren);
