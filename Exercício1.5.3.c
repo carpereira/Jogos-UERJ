83cd990985678fc6190e3abff0fc34f51990a11b
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-04 21:30:28 -0300

    Update Exercício1.5.3.c

--- Exercício1.5.3.c
+++ Exercício1.5.3.c
@@ -1,93 +1,52 @@
 #include <SDL2/SDL.h>
 
-int main(int argc, char* args[])
+int main (int argc, char* args[])
 {
-/*INICIALIZAÇÃO*/
-SDL_Init(SDL_INIT_EVERYTHING);
-SDL_Window* win = SDL_CreateWindow("Animação Orientada a Eventos!",
-                      SDL_WINDOWPOS_UNDEFINED,
-                      SDL_WINDOWPOS_UNDEFINED,
-                      300, 300, SDL_WINDOW_SHOWN
+    /* INICIALIZACAO */
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* win = SDL_CreateWindow("Animando e Movendo um Retângulo",
+                         SDL_WINDOWPOS_UNDEFINED,
+                         SDL_WINDOWPOS_UNDEFINED,
+                         200, 100, SDL_WINDOW_SHOWN
                       );
-SDL_Renderer* ren = SDL_CreateRenderer(win, -1,0);
+    SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
 
-/*EXECUÇÃO*/
-
-SDL_Rect r={40,20,10,10};
-SDL_Rect s={280,280,10,10};
-  
-  
-    while (1) {        
-        SDL_SetRenderDrawColor(ren, 255,255,255,0);
+    /* EXECUÇÃO */
+    SDL_Rect r = { 40,20, 10,10 };
+    while (1) {
+        SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
         SDL_RenderClear(ren);
-        SDL_SetRenderDrawColor(ren, 0,0,255,0);
-        SDL_RenderFillRect(ren, &r);       
-        //SDL_SetRenderDrawColor(ren, 255,0,0,0);
-        //SDL_RenderFillRect(ren, &s);  
+        SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+        SDL_RenderFillRect(ren, &r);
         SDL_RenderPresent(ren);
-      
-      SDL_Event evt;
-      
-      if (evt.type == SDL_QUIT){            
-            break;
-        } 
-      
-      int isevt = SDL_WaitEventTimeout(&evt, 100);
-        if (isevt) {
-          if (r.y<280){
-            SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-            SDL_RenderClear(ren);
-            SDL_SetRenderDrawColor(ren, 0x00,0xFF,0x00,0x00);
-            SDL_RenderFillRect(ren,&r);      
-            SDL_RenderPresent(ren);}
-        }
-      else{
-            r.y +=1;}
-        }                                
-         int isevt = SDL_WaitEventTimeout(&evt, 100);
-         if (isevt) { 
-           if (r.x<280){
-             SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-             SDL_RenderClear(ren);
-             SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
-             SDL_RenderFillRect(ren,&r);             
-             SDL_RenderPresent(ren);}
-         }
-        else{
-          r.x +=1;}
-        }  
-                            
-              
-      /*if (isevt) {
-           if (s.y>20){
-              SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-              SDL_RenderClear(ren);
-              SDL_SetRenderDrawColor(ren, 0x00,0xFF,0xFF,0x00);
-              SDL_RenderFillRect(ren,&s);      
-              SDL_RenderPresent(ren);}
-       } else{
-              s.y -=1;
-              }
-      if (isevt) {
-           if (s.x>20){
-             SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-             SDL_RenderClear(ren);
-             SDL_SetRenderDrawColor(ren, 0xFF,0x00,0x00,0x00);
-             SDL_RenderFillRect(ren,&s);      
-             SDL_RenderPresent(ren);}
-       } else{
-              s.x -=1;
-              }*/
-  
-    }          
-              
-        
-  
-    
-  
 
-/*FINALIZAÇÃO*/
-SDL_DestroyRenderer(ren);
-SDL_DestroyWindow(win);
-SDL_Quit();
+        SDL_Event evt;
+        int isevt = SDL_WaitEventTimeout(&evt, 500);
+        if (isevt) {
+            if (evt.type == SDL_KEYDOWN) {
+                switch (evt.key.keysym.sym) {
+                    case SDLK_UP:
+                        r.y -= 5;
+                        break;
+                    case SDLK_DOWN:
+                        r.y += 5;
+                        break;
+                    case SDLK_LEFT:
+                        r.x -= 5;
+                        break;
+                    case SDLK_RIGHT:
+                        r.x += 5;
+                        break;
+                }
+            }
+        } else {
+            r.x += 2;
+            r.y += 2;
+        }
+    }
+
+    /* FINALIZACAO */
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
 }
