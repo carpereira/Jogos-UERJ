7f6ff1faf343d91cd0d9232c95ca6af81f83002a
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-04 22:51:55 -0300

    Update Exercício1.5.3.c

--- Exercício1.5.3.c
+++ Exercício1.5.3.c
@@ -70,11 +70,11 @@ int main (int argc, char* args[])
                         break;}
                 }
             //}
-        } //else {
+        //} //else {
             //r.x += 2;
             //r.y += 2;
         //}
-    }
+    //}
 
     /* FINALIZACAO */
     SDL_DestroyRenderer(ren);
