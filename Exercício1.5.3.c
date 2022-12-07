0e6ae38dce14ac3fdbf040175da5d01fcb631b46
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-07 11:06:44 -0300

    Update Exercício1.5.3.c

--- Exercício1.5.3.c
+++ Exercício1.5.3.c
@@ -42,7 +42,7 @@ int main (int argc, char* args[])
                         SDL_RenderPresent(ren);
                         r.y += 2;
                         break;}
-                    /*case 2:
+                    case 2:
                         if (r.x<280){
                         SDL_SetRenderDrawColor(ren, 255,255,255,0);
                         SDL_RenderClear(ren);
@@ -51,7 +51,7 @@ int main (int argc, char* args[])
                         SDL_RenderPresent(ren);
                         r.x += 2;
                         break;}
-                    case 3:
+                    /*case 3:
                         if (r.y>20){
                         SDL_SetRenderDrawColor(ren, 255,255,255,0);
                         SDL_RenderClear(ren);
