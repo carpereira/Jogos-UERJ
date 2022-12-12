d514e0df7b3fb271b87a1c6a6b0d8140404b00ae
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-12 17:32:17 -0300

    Update Exercício1.4.1.c

--- Exercício1.4.1.c
+++ Exercício1.4.1.c
@@ -34,7 +34,7 @@ int main (int argc, char* args[])
         //int isevt = SDL_WaitEevntTimeout(&evt, 1000);
         //if (isevt){
             if (evt.type == SDL_MOUSEBUTTONDOWN){
-                while(SDL_PollEvent(&evt){
+                while(SDL_WaitEvent(&evt){
                 switch(1){
                     /*case 1:
                         r.x=evt.motion.x, r.y=evt.motion.y;   
