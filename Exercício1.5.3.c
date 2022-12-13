d43a32cb8b50542a0493d47e8926bb8e9832c0c3
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-13 17:05:32 -0300

    Update Exercício1.5.3.c

--- Exercício1.5.3.c
+++ Exercício1.5.3.c
@@ -82,7 +82,7 @@ int main (int argc, char* args[])
                             break;
                             } 
             }
-        //}   
+        }   
                 }
     }
     /* FINALIZACAO */
