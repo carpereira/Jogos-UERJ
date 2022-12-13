7caeeae63137ad81d6f27fad04f1f46b1f9ca96e
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-13 00:36:12 -0300

    Update Exercício1.4.1.c

--- Exercício1.4.1.c
+++ Exercício1.4.1.c
@@ -110,7 +110,7 @@ int main (int argc, char* args[])
                       //}                   
             //}
         //}
-    //}
+    }
    
     /* FINALIZACAO */
     SDL_DestroyRenderer(ren);
