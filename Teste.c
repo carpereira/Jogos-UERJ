5fce3b71154526a8426a2b35b6575e6ac5516fbc
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-14 18:51:53 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -33,7 +33,7 @@ int main (int argc, char* args[])
         switch(evt.type){
             case 1:
                 if (evt.type == SDL_MOUSEBUTTONDOWN){                
-                (r.x=evt.motion.x, r.y=evt.motion.y);   
+                r.x=evt.motion.x, r.y=evt.motion.y;   
                 SDL_SetRenderDrawColor(ren, 255,255,255,0);            
                 SDL_SetRenderDrawColor(ren, 255,0,0,0);            
                 SDL_RenderFillRect(ren, &r);
