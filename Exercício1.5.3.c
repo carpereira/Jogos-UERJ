fd5851e72c906b84949ae618a783e787b6d16b8d
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-13 23:17:47 -0300

    Update Exercício1.5.3.c

--- Exercício1.5.3.c
+++ Exercício1.5.3.c
@@ -38,7 +38,7 @@ int main (int argc, char* args[])
         int isevt = SDL_WaitEventTimeout(&evt, 500);
         //int isevt = SDL_WaitEvent(&evt);
         if (isevt) {            
-            if (evt.type == SDL_KEYDOWN) { 
+            if (evt.type == SDL_MOUSEBUTTONDOWN) { 
                 //switch (if (!= SDL_QUIT)) {
                 switch (1) {
                     case 1:
