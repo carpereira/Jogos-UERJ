00c3196055e74f1995538996a7ec564803e3071b
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-14 00:10:50 -0300

    Update Exercício1.5.3.c

--- Exercício1.5.3.c
+++ Exercício1.5.3.c
@@ -16,7 +16,7 @@ int main (int argc, char* args[])
     SDL_Rect s = {280,280,20,20};
     SDL_Rect t = {150,150,20,20};
     
-        SDL_SetRenderDrawColor(ren, 255,255,255,1000);
+        SDL_SetRenderDrawColor(ren, 255,255,255,0);
         SDL_RenderClear(ren);
         SDL_SetRenderDrawColor(ren, 0,0,255,0);
         SDL_RenderFillRect(ren, &r);
@@ -38,7 +38,7 @@ int main (int argc, char* args[])
             break;
         } 
         
-        int isevt = SDL_WaitEventTimeout(&evt, 0);
+        int isevt = SDL_WaitEventTimeout(&evt, 1000);
         //int isevt = SDL_WaitEvent(&evt);
         //int isevt = SDL_Event(&evt);
         if (isevt) {
