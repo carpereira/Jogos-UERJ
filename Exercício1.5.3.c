c5479888324102ae2bd3f7e4e71f78070b6578d2
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-13 22:32:25 -0300

    Update Exercício1.5.3.c

--- Exercício1.5.3.c
+++ Exercício1.5.3.c
@@ -81,22 +81,7 @@ int main (int argc, char* args[])
                         break;                    
             }
         }   
-        if (evt.type == SDL_KEYDOWN) {
-            switch (evt.key.keysym.sym) {
-                case SDLK_UP:
-                    r.y -= 5;
-                    break;
-                case SDLK_DOWN:
-                    r.y += 5;
-                    break;
-                case SDLK_LEFT:
-                    r.x -= 5;
-                    break;
-                case SDLK_RIGHT:
-                    r.x += 5;
-                    break;
-            }
-        }
+        
                 //}
     }
     /* FINALIZACAO */
