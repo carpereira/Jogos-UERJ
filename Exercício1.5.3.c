256693c0d191e1348962e1ef2a1dc30d31e7aa83
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-13 22:03:13 -0300

    Update Exercício1.5.3.c

--- Exercício1.5.3.c
+++ Exercício1.5.3.c
@@ -33,7 +33,7 @@ int main (int argc, char* args[])
         //int isevt = SDL_WaitEventTimeout(&evt, 500);
         int isevt = SDL_WaitEvent(&evt);
         if (isevt) {            
-            if (evt.type == SDL_KEYDOWN) { 
+            //if (evt.type == SDL_KEYDOWN) { 
                 //switch (if (!= SDL_QUIT)) {
                 switch (1) {
                     case 1:
@@ -81,7 +81,7 @@ int main (int argc, char* args[])
                         break;                    
             }
         }   
-                }
+                //}
     }
     /* FINALIZACAO */
     SDL_DestroyRenderer(ren);
