49e2e928141107d3a5163484bc4cd7f2f49167ae
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-14 20:26:14 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -43,7 +43,7 @@ int main (int argc, char* args[])
                 SDL_SetRenderDrawColor(ren, 255,0,0,0);            
                 SDL_RenderFillRect(ren, &r);
                 SDL_RenderPresent(ren);
-                break;
+                break;}
         }     
         
         
@@ -56,7 +56,7 @@ int main (int argc, char* args[])
                 SDL_SetRenderDrawColor(ren, 0,255,0,0);            
                 SDL_RenderFillRect(ren, &r);            
                 SDL_RenderPresent(ren);                
-                break;
+                break;}
         }  
         
         if(evt.type == SDL_MOUSEBUTTONDOWN){
@@ -68,27 +68,31 @@ int main (int argc, char* args[])
                 SDL_SetRenderDrawColor(ren, 0,255,255,0);            
                 SDL_RenderFillRect(ren, &r);            
                 SDL_RenderPresent(ren);                
-               break;
+               break;}
         }
         
-        switch(evt.type == SDL_MOUSEBUTTONDOWN){
+        if(evt.type == SDL_MOUSEBUTTONDOWN){
+            switch(1){
+        //switch(evt.type == SDL_MOUSEBUTTONDOWN){
             case 1:               
                 r.x=evt.motion.x, r.y=evt.motion.y;
                 SDL_SetRenderDrawColor(ren, 255,255,255,0);             
                 SDL_SetRenderDrawColor(ren, 255,0,255,0);            
                 SDL_RenderFillRect(ren, &r);            
                 SDL_RenderPresent(ren);
-                break;
+                break;}
             }
         
-        switch(evt.type == SDL_MOUSEBUTTONDOWN){
+        if(evt.type == SDL_MOUSEBUTTONDOWN){
+            switch(1){
+        //switch(evt.type == SDL_MOUSEBUTTONDOWN){
             case 1:               
                 r.x=evt.motion.x, r.y=evt.motion.y;
                 SDL_SetRenderDrawColor(ren, 255,255,255,0);            
                 SDL_SetRenderDrawColor(ren, 255,150,0,0);            
                 SDL_RenderFillRect(ren, &r);            
                 SDL_RenderPresent(ren);
-                break;
+                break;}
         }
       /*  
         switch(evt.type){
