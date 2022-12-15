fd5fb7f4c6c97365b1aabb9de1a03f759fb5b813
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-14 22:18:22 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -37,7 +37,7 @@ int main (int argc, char* args[])
                 SDL_SetRenderDrawColor(ren, 255,0,0,0);            
                 SDL_RenderFillRect(ren, &r);
                 SDL_RenderPresent(ren);
-                break;
+                //break;
         }     
                
         if (evt.type == SDL_MOUSEBUTTONDOWN){                            
@@ -46,7 +46,7 @@ int main (int argc, char* args[])
                 SDL_SetRenderDrawColor(ren, 0,255,0,0);            
                 SDL_RenderFillRect(ren, &r);            
                 SDL_RenderPresent(ren);                
-                break;
+                //break;
         }  
                 
         if (evt.type == SDL_MOUSEBUTTONDOWN){                           
@@ -55,7 +55,7 @@ int main (int argc, char* args[])
                 SDL_SetRenderDrawColor(ren, 0,255,255,0);            
                 SDL_RenderFillRect(ren, &r);            
                 SDL_RenderPresent(ren);                
-               break;
+                //break;
         }
         /*
         //if(evt.type == SDL_MOUSEBUTTONDOWN){
