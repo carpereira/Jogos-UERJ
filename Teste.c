ab9527d28a41ec4128172b56c5f5edff37aa0af6
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-14 21:46:27 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -31,7 +31,7 @@ int main (int argc, char* args[])
             break;
         }
                 
-        if (switch(evt.type)){
+        switch(evt.type){
             case SDL_MOUSEBUTTONDOWN:                        
                 r.x=evt.motion.x, r.y=evt.motion.y;   
                 SDL_SetRenderDrawColor(ren, 255,255,255,0);            
@@ -41,7 +41,7 @@ int main (int argc, char* args[])
                 break;
         }     
                
-        if (switch(evt.type)){
+        switch(evt.type){
             case SDL_MOUSEBUTTONDOWN:                           
                 r.x=evt.motion.x, r.y=evt.motion.y;
                 SDL_SetRenderDrawColor(ren, 255,255,255,0);            
@@ -51,7 +51,7 @@ int main (int argc, char* args[])
                 break;
         }  
                 
-        if (switch(evt.type)){
+        switch(evt.type){
             case SDL_MOUSEBUTTONDOWN:                          
                 r.x=evt.motion.x, r.y=evt.motion.y;
                 SDL_SetRenderDrawColor(ren, 255,255,255,0);            
