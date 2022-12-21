985121cb85852c697196e223d262d689ba7a2905
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-21 20:53:55 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -16,10 +16,11 @@ int main (int argc, char* args[])
     /* EXECUÇÃO */
     SDL_Rect r = { 100,100, 10,10 };
     
-    SDL_SetRenderDrawColor(ren, 255,255,255,0);
-    SDL_RenderClear(ren);
+    
          
     SDL_Event evt;
+    SDL_SetRenderDrawColor(ren, 255,255,255,0);
+    SDL_RenderClear(ren);
     
     while (1) {                      
         SDL_WaitEvent(&evt);    
