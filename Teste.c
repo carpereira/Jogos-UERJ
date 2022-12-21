023531cce119efabe07438e60c08503906d1d5d0
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-21 18:24:17 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -20,7 +20,7 @@ int main (int argc, char* args[])
     
     while (1) {                      
         SDL_WaitEvent(&evt);    
-        /*if (evt.type == SDL_MOUSEBUTTONDOWN){                        
+        if (evt.type == SDL_MOUSEBUTTONDOWN){                        
                 r.x=evt.motion.x, r.y=evt.motion.y;   
                 SDL_SetRenderDrawColor(ren, 255,255,255,0);
                 SDL_RenderClear(ren);
@@ -29,7 +29,7 @@ int main (int argc, char* args[])
                 SDL_RenderPresent(ren); 
                 //break;        
         }   
-      }
+      /*}
        
     while (1) {                     
         SDL_WaitEvent(&evt);   
