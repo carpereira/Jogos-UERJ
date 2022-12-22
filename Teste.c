de334d68726d716f3fa070866b4efe88c5228952
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-22 00:44:43 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -38,7 +38,7 @@ int main (int argc, char* args[])
         SDL_Event evt;
         int isevt = SDL_WaitEventTimeout(&evt,500);
         if (isevt){
-        while(quit=false){
+        if (quit=false){
             int i;
             for(i=1;i<=max;i++){
                 switch (i) {
