41e70e26e341c379de3cccec72452876f5ed8039
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-23 18:03:48 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -35,10 +35,10 @@ int main (int argc, char* args[])
             break;
         }         
      
-        SDL_WaitEvent (&evt);
-        int isevt = SDL_WaitEventTimeout(&evt,5000);
+        /*SDL_WaitEvent (&evt);
+        int isevt = SDL_WaitEventTimeout(&evt,500);
         if (isevt){
-            //if (evt.type == SDL_KEYDOWN){
+            //if (evt.type == SDL_KEYDOWN){*/
         while (quit=false){
             int i;
             for(i=1;i<=max;i++){
@@ -87,8 +87,8 @@ int main (int argc, char* args[])
                         SDL_RenderPresent(ren);                        
                         break;
                 //}
-                }
-                }
+             //  }
+               // }
             }
         }   
     }     
