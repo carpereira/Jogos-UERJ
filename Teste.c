2c295b7e750a2cccb7a84123fb208d4447936922
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-27 20:00:38 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -38,7 +38,7 @@ int main (int argc, char* args[])
      
         SDL_Event evt;
         //SDL_WaitEvent(&evt);
-        int isevt = SDL_WaitEventTimeout(&evt,1000000);
+        int isevt = SDL_WaitEventTimeout(&evt,10);
         if (isevt){
             //if (evt.type == SDL_KEYDOWN){
             if (evt.type == SDL_MOUSEBUTTONDOWN){
