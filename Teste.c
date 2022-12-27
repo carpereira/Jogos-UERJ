ed4d528bdd44625cd91f85ed08d9940ccc93df8c
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-27 18:04:04 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -38,10 +38,10 @@ int main (int argc, char* args[])
             break;
         }         
      
-        /*SDL_Event evt;
+        SDL_Event evt;
         int isevt = SDL_WaitEventTimeout(&evt,500);
         if (isevt){
-            if (evt.type == SDL_KEYDOWN){*/
+            //if (evt.type == SDL_KEYDOWN){
         while (quit=false){
             int i;
             for(i=1;i<=max;i++){
