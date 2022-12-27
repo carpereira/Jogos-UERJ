7299c3da859d2f9866e494d3b8aa8f2dfaf13678
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-27 18:04:37 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -90,7 +90,7 @@ int main (int argc, char* args[])
                         SDL_RenderPresent(ren);                        
                         break;
                 //}
-                //}
+                }
              }
                }
             }
