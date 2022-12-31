87b87bbb31101a19bd04642abb6b168b48178fcc
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-31 17:02:16 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -40,7 +40,7 @@ int main (int argc, char* args[])
             if(evt.type == SDL_MOUSEBUTTONDOWN){
                 SDL_SetRenderDrawColor(ren, 255,255,255,0);
                 SDL_RenderClear(ren);
-                SDL_SetRenderDrawColor(ren, 255,0,0,0);
+                SDL_SetRenderDrawColor(ren, 0,0,255,0);
                 SDL_RenderFillRect(ren, &r);
                 
                 SDL_SetRenderDrawColor(ren, 0,255,0,0);
