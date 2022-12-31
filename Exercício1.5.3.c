e00874fd51555309b36da5373596eb526a855870
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-31 09:40:36 -0300

    Update Exercício1.5.3.c

--- Exercício1.5.3.c
+++ Exercício1.5.3.c
@@ -93,7 +93,7 @@ int main (int argc, char* args[])
                         SDL_RenderPresent(ren);                        
                         break;                
             }
-               }
+               //}
             }
         }   
     }     
