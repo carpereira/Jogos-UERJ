6c80b551dfc7d7493271fb1f317c348bf09541b8
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-31 09:30:10 -0300

    Update Exercício1.5.3.c

--- Exercício1.5.3.c
+++ Exercício1.5.3.c
@@ -22,7 +22,7 @@ int main (int argc, char* args[])
     
     while(1){
     
-    //SDL_Event evt;
+    SDL_Event evt;
     SDL_SetRenderDrawColor(ren, 255,255,255,0);
     SDL_RenderClear(ren);
     SDL_SetRenderDrawColor(ren, 0,0,255,0);
@@ -38,7 +38,7 @@ int main (int argc, char* args[])
             break;
         }         
      
-        SDL_Event evt;        
+        //SDL_Event evt;        
         int isevt = SDL_WaitEventTimeout(&evt,500);
         if (isevt){            
             if (evt.type == SDL_MOUSEBUTTONDOWN){     
