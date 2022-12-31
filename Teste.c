e071f4119085fc124b79633726822a01b3a21e9e
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-31 16:24:05 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -34,7 +34,7 @@ int main (int argc, char* args[])
         SDL_RenderPresent(ren);
         
         SDL_Event evt;
-        while (r.y>35 | s.y>35 | t.y>35){
+        if (r.y>35 | s.y>35 | t.y>35){
         int isevt = SDL_WaitEventTimeout(&evt,500);
         if(isevt){
             if(evt.type == SDL_MOUSEBUTTONDOWN){
