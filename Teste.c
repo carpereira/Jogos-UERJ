6cce978221ff2e3964754f1978bc06d3f80ab8bf
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-31 15:06:34 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -52,9 +52,13 @@ int main (int argc, char* args[])
             }
         }
             else{
-                if(r.y>30 & s.y>30 & t.y>30){
-                r.y -=5;
-                s.y -=4;
+                if(r.y>30{
+                    r.y -=5;
+                }
+                if(s.y>30{
+                    s.y -=4
+                    }
+                if(t.y>30){
                 t.y -=2;}
             }                
               
