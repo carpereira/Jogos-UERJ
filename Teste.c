5ae9f86a673f473bf3432f16682b417e60a74abe
carpereira <59072054+carpereira@users.noreply.github.com>
2022-12-31 13:57:45 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -85,7 +85,7 @@ int main (int argc, char* args[])
                     break;}
                     else {t.x=150;
                         break;}                    
-            //}
+            }
         }
     }
         
