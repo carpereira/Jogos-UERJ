cb5cf015e19d57b51d4b02cc8521230e770542ad
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-01 18:12:17 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -15,8 +15,8 @@ int main (int argc, char* args[])
 
     /* EXECUÇÃO */
     SDL_Rect r = { 80,280, 20,20 }; 
-    SDL_Rect s = { 160,280, 20,20 };
-    SDL_Rect t = { 240,280, 20,20 };
+    SDL_Rect s = { 160,260, 30,40 };
+    SDL_Rect t = { 240,280, 50,20 };
     SDL_Rect w = {0,30,300,5};
    
     while (1) {        
