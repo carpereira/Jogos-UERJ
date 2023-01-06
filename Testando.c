af94cb19bb314c70da8f9542ba08cf49692aee91
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-05 23:37:57 -0300

    Update Testando.c

--- Testando.c
+++ Testando.c
@@ -1,41 +1,64 @@
-#include <assert.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
+// Testado com MinGW64:
+// gcc -Wall -Wextra -Wpedantic -Wno-unused-parameter test1.c `sdl2-config --cflags --libs` -lSDL2_ttf
+#include <SDL.h>
+#include <SDL_ttf.h>
 
-int main (int argc, char* args[])
-{
-    /* INICIALIZACAO */
-    SDL_Init(SDL_INIT_EVERYTHING);
+SDL_Window   *window;
+SDL_Renderer *renderer;
+TTF_Font     *font;
+
+#define MAX_LENGTH 1024
+void drawText(const char* fmt, ...) {
+    char buffer[MAX_LENGTH] = {0};
+
+    va_list ap;
+
+    va_start (ap, fmt);
+
+    SDL_vsnprintf(buffer, MAX_LENGTH, fmt, ap);
+
+    va_end (ap);
+
+    SDL_Surface* text = TTF_RenderText_Solid(font, buffer,
+                    (SDL_Color) { 255, 255, 255, 255 });
+    
+    SDL_Texture* textureText = SDL_CreateTextureFromSurface(renderer, text);
+
+    SDL_Rect dest = {0};
+    SDL_QueryTexture(textureText, NULL, NULL, &dest.w, &dest.h);
+
+    SDL_FreeSurface(text);
+
+    SDL_RenderCopy(renderer, textureText, NULL, &dest);
+
+    SDL_DestroyTexture(textureText);
+}
+
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
     TTF_Init();
-    SDL_Window* win = SDL_CreateWindow("Texto",
-                         SDL_WINDOWPOS_UNDEFINED,
-                         SDL_WINDOWPOS_UNDEFINED,
-                         400, 200, SDL_WINDOW_SHOWN
-                      );
-    SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
 
-    TTF_Font* fnt = TTF_OpenFont("tiny.ttf", 20);
-    assert(fnt != NULL);
-    SDL_Color clr = {0xFF,0x00,0x00,0xFF};
-    SDL_Surface* sfc = TTF_RenderText_Blended(fnt, "ola mundo", clr);
-    assert(sfc != NULL);
-    SDL_Texture* txt = SDL_CreateTextureFromSurface(ren, sfc);
-    assert(txt != NULL);
-    SDL_FreeSurface(sfc);
+    // https://fonts.google.com/specimen/VT323
+    font = TTF_OpenFont("VT323.ttf", 24);
 
-    /* EXECUÇÃO */
-    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-    SDL_RenderClear(ren);
-    SDL_Rect r = { 50,50, 200,80 };
-    SDL_RenderCopy(ren, txt, NULL, &r);
-    SDL_RenderPresent(ren);
-    SDL_Delay(5000);
+    window = SDL_CreateWindow("Window Title", SDL_WINDOWPOS_CENTERED,
+                    SDL_WINDOWPOS_CENTERED, 360, 240, 0);
 
-    /* FINALIZACAO */
-    SDL_DestroyTexture(txt);
-    TTF_CloseFont(fnt);
-    SDL_DestroyRenderer(ren);
-    SDL_DestroyWindow(win);
-    TTF_Quit();
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+
+    int counter = 0;
+    while (!SDL_QuitRequested()) {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        drawText("Your Score: %d", counter++);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    TTF_CloseFont(font);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
+    return 0;
 }
