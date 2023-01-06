b6a73bba34265e4b092bf480d2a80ee425a608ed
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-05 22:08:55 -0300

    Update Exercício1.6.c

--- Exercício1.6.c
+++ Exercício1.6.c
@@ -1,10 +1,13 @@
 #include <SDL2/SDL.h>
+#include <stdlib.h>
+#include <SDL_ttf.h>
 
 
 int main (int argc, char* args[])
 {
     /* INICIALIZACAO */
     SDL_Init(SDL_INIT_EVERYTHING);
+    TTF_Int();
     SDL_Window* win = SDL_CreateWindow("Linha de chegada com Tres Retângulos",
                          SDL_WINDOWPOS_UNDEFINED,
                          SDL_WINDOWPOS_UNDEFINED,
@@ -14,9 +17,9 @@ int main (int argc, char* args[])
     
 
     /* EXECUÇÃO */
-    SDL_Rect r = { 80,280, 20,20 }; 
-    SDL_Rect s = { 160,280, 20,20 };
-    SDL_Rect t = { 240,280, 20,20 };
+    SDL_Rect r = { 50,280, 20,20 }; 
+    SDL_Rect s = { 140,260, 30,40 };
+    SDL_Rect t = { 240,280, 50,20 };
     SDL_Rect w = {0,30,300,5};
    
     while (1) {        
@@ -40,26 +43,31 @@ int main (int argc, char* args[])
             if(evt.type == SDL_MOUSEBUTTONDOWN){
                 SDL_SetRenderDrawColor(ren, 255,255,255,0);
                 SDL_RenderClear(ren);
-                SDL_SetRenderDrawColor(ren, 255,0,0,0);
-                SDL_RenderFillRect(ren, &r);
-                
+                SDL_SetRenderDrawColor(ren, 0,0,255,0);
+                SDL_RenderFillRect(ren, &r);                
                 SDL_SetRenderDrawColor(ren, 0,255,0,0);
-                SDL_RenderFillRect(ren, &s);
-                
+                SDL_RenderFillRect(ren, &s);                
                 SDL_SetRenderDrawColor(ren, 255,0,0,0);
                 SDL_RenderFillRect(ren, &t);
-                SDL_RenderPresent(ren);                
+                SDL_RenderPresent(ren);               
             }
         }
             else{
                 if(r.y>35){
-                    r.y -=5;
+                    r.y -=5;}
+                    else{
+                        printf("Azul");
                 }
                 if(s.y>35){
-                    s.y -=4;
+                    s.y -=4;}
+                    else{
+                        printf("Verde");
                     }
                 if(t.y>35){
                 t.y -=2;}
+                else{
+                    printf("Vermelho");
+            }
             }
         }
         else{
@@ -79,10 +87,47 @@ int main (int argc, char* args[])
         if (evt.type == SDL_QUIT){
             break;
         }        
+        
+        if(evt.type ==  SDL_MOUSEMOTION){
+            if(s.y>35){
+                s.x=evt.motion.x, s.y=evt.motion.y;}
+            else{s.y=35;                 
+            }
+        }
+        if  (evt.type == SDL_KEYDOWN){
+            switch (evt.key.keysym.sym){
+                case SDLK_UP:
+                    if(t.y>35){
+                        t.y -=5;
+                        break;}
+                    else {t.y=35;
+                          break;}
+                case SDLK_DOWN:
+                    if(t.y<280){
+                        t.y +=5;
+                        break;}
+                    else {t.y=280;
+                          break;}
+               case SDLK_LEFT:
+                    if(t.x>0){
+                        t.x -=5;
+                        break;}
+                    else {t.x=0; 
+                          break;}
+               case SDLK_RIGHT:
+                    if(t.x<250){
+                        t.x +=5;
+                        break;}
+                    else {t.x=250;
+                          break;}
+    }
+        }
     }
         
     /* FINALIZACAO */
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
     SDL_Quit();
+    TTF_CloseFont(font);
 }
+ 
