31623ad422c4e5f7b2b84f9d7a9ffa1c2f8ca0b1
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-07 15:16:04 -0300

    Update Exercício2.0.c

--- Exercício2.0.c
+++ Exercício2.0.c
@@ -1,41 +1,64 @@
-const draggable = document.getElementById("myObject");
-const setStatus = (str) =>
-  (document.getElementById("status").textContent = str);
-
 let didDrag = false;
+let didHandlePointerDown = false;
 let dragOrigin;
 let origPos;
 
-draggable.addEventListener("pointerdown", (evt) => {
-  evt.target.setPointerCapture(evt.pointerId);
-  dragOrigin = { x: evt.clientX, y: evt.clientY };
-  const { left, top } = draggable.getBoundingClientRect();
-  origPos = { left, top };
-  didDrag = false;
-});
-draggable.addEventListener("pointermove", (evt) => {
-  if (dragOrigin) {
-    didDrag = true;
-    const deltaX = evt.clientX - dragOrigin.x;
-    const deltaY = evt.clientY - dragOrigin.y;
-    draggable.style.left = `${origPos.left + deltaX}px`;
-    draggable.style.top = `${origPos.top + deltaY}px`;
-    setStatus("dragging...");
-  }
-});
-draggable.addEventListener("pointerup", (evt) => {
-  dragOrigin = undefined;
-  if (didDrag) {
-    setStatus("Dropped!");
+const rectContains = ({ top, right, bottom, left }, x, y) =>
+  left <= x && x <= right && top <= y && y <= bottom;
+
+(function handleFrame() {
+  if (mouse.buttons[0].pressed) {
+    const { clientX, clientY } = mouse.location;
+    const draggableRect = draggable.getBoundingClientRect();
+
+    if (
+      !didHandlePointerDown &&
+      rectContains(draggableRect, clientX, clientY)
+    ) {
+      // Handle pointerdown
+      dragOrigin = { x: clientX, y: clientY };
+      origPos = {
+        left: draggableRect.left,
+        top: draggableRect.top,
+      };
+      didDrag = false;
+    }
+
+    // Ensure that we only act on pointerdown action in the
+    // first frame that we detect that the button is pressed.
+    didHandlePointerDown = true;
+
+    if (
+      dragOrigin &&
+      (clientX !== dragOrigin.x || clientY !== dragOrigin.y)
+    ) {
+      // Handle pointermove
+      didDrag = true;
+      const deltaX = clientX - dragOrigin.x;
+      const deltaY = clientY - dragOrigin.y;
+      draggable.style.left = `${origPos.left + deltaX}px`;
+      draggable.style.top = `${origPos.top + deltaY}px`;
+      setStatus("dragging...");
+    }
+  } else if (dragOrigin) {
+    // Handle pointerup
+    dragOrigin = undefined;
+    if (didDrag) {
+      setStatus("Dropped!");
+    } else {
+      setStatus("Clicked!");
+    }
   } else {
-    setStatus("Clicked!");
+    didHandlePointerDown = false;
   }
-});
-window.addEventListener("keydown", (evt) => {
-  if (evt.key === "Escape") {
+
+  if (dragOrigin && keyboard.keys["Escape"].pressed) {
+    // Handle keypress
     dragOrigin = undefined;
     draggable.style.left = `${origPos.left}px`;
     draggable.style.top = `${origPos.top}px`;
     setStatus("Cancelled!");
   }
-});
+
+  requestAnimationFrame(handleFrame);
+})();
