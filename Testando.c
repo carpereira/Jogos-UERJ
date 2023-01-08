5517a34a85612118e17c8caa09dffc5f6109090c
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-08 13:43:22 -0300

    Update Testando.c

--- Testando.c
+++ Testando.c
@@ -1,25 +1,36 @@
-#include <SDL2/SDL_ttf.h>
-#include <assert.h>
-int main (int argc, char* args[]) {
-/* INICIALIZACAO */
-TTF_Init();
-TTF_Font* fnt = TTF_OpenFont("tiny.ttf", 20);
-assert(fnt != NULL);
-SDL_Color clr = {0xFF,0x00,0x00,0xFF};
-SDL_Surface* sfc = TTF_RenderText_Blended(fnt, "ola mundo", clr);
-assert(sfc != NULL);
-SDL_Texture* txt = SDL_CreateTextureFromSurface(ren, sfc);
-assert(txt != NULL);
-SDL_FreeSurface(sfc);
-/* EXECUÇÃO */
-SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-SDL_RenderClear(ren);
-SDL_Rect r = { 50,50, 200,80 };
-SDL_RenderCopy(ren, txt, NULL, &r);
-SDL_RenderPresent(ren);
-SDL_Delay(5000);
-/* FINALIZACAO */
-SDL_DestroyTexture(txt);
-TTF_CloseFont(fnt);
-TTF_Quit();
+// Testado com MinGW64:
+// gcc -Wall -Wextra -Wpedantic -Wno-unused-parameter test0.c `sdl2-config --cflags --libs` -lSDL2_ttf
+#include <SDL.h>
+#include <SDL_ttf.h>
+
+SDL_Window  *window;
+SDL_Surface *surface;
+TTF_Font    *font;
+
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init();
+
+    // https://fonts.google.com/specimen/VT323
+    font = TTF_OpenFont("VT323.ttf", 24);
+
+    window = SDL_CreateWindow("Window Title", SDL_WINDOWPOS_CENTERED,
+                    SDL_WINDOWPOS_CENTERED, 360, 240, 0);
+
+    surface = SDL_GetWindowSurface(window);
+
+    while (!SDL_QuitRequested()) {
+        SDL_Surface* text = TTF_RenderText_Solid(font, "Hello, World!",
+                    (SDL_Color) { 255, 255, 255, 255 });
+
+        SDL_FillRect(surface, NULL, 0);
+        SDL_BlitSurface(text, NULL, surface, NULL);
+        SDL_FreeSurface(text);
+        SDL_UpdateWindowSurface(window);
+    }
+
+    TTF_CloseFont(font);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
