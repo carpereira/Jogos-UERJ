118c83a2715fcf3759363aa395989c25129fc78b
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-10 17:43:00 -0300

    Update Exercício1.4.1.c

--- Exercício1.4.1.c
+++ Exercício1.4.1.c
@@ -137,14 +137,14 @@ int main (int argc, char* args[])
                 SDL_RenderPresent(ren);
                 //j++;
         }  
-        }
-        else{
+        //}
+        /*else{
             SDL_SetRenderDrawColor(ren, 255,255,255,0);
             SDL_RenderClear(ren);
             SDL_SetRenderDrawColor(ren, 0,0,255,0);
             SDL_RenderFillRect(ren, &r);       
             SDL_RenderPresent(ren); 
-        }
+        }*/
         }
         //SDL_WaitEvent(&evt);  
         if (evt.type == SDL_QUIT){
