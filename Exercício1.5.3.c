cc09ff596ffe3f0f7e50572d1a1d849a0157a670
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-10 17:12:43 -0300

    Update Exercício1.5.3.c

--- Exercício1.5.3.c
+++ Exercício1.5.3.c
@@ -43,7 +43,7 @@ int main (int argc, char* args[])
                         SDL_SetRenderDrawColor(ren, 255,0,0,0);
                         SDL_RenderFillRect(ren,&r);
                         SDL_RenderPresent(ren);
-                        SDL_Delay(100);
+                        //SDL_Delay(100);
                         r.y += 5;}
                         break;
                     case 2:
@@ -53,7 +53,7 @@ int main (int argc, char* args[])
                         SDL_SetRenderDrawColor(ren, 0,255,0,0);
                         SDL_RenderFillRect(ren,&r);
                         SDL_RenderPresent(ren);
-                        SDL_Delay(100);
+                        //SDL_Delay(100);
                         r.x += 5;}
                         break;
                     case 3:
@@ -63,7 +63,7 @@ int main (int argc, char* args[])
                         SDL_SetRenderDrawColor(ren, 0,0,255,0);
                         SDL_RenderFillRect(ren,&s);
                         SDL_RenderPresent(ren);
-                        SDL_Delay(100);
+                        //SDL_Delay(100);
                         s.y -= 5;}
                         break;
                     case 4:
@@ -73,7 +73,7 @@ int main (int argc, char* args[])
                         SDL_SetRenderDrawColor(ren, 0,255,255,0);
                         SDL_RenderFillRect(ren,&s);
                         SDL_RenderPresent(ren);
-                        SDL_Delay(100);
+                        //SDL_Delay(100);
                         s.x -= 5;}
                         break; 
                     case 5:                        
