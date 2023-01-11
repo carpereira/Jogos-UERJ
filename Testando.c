552c8d0c1d2ab29e59c72268544262d1e5a54dc9
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-11 20:48:31 -0300

    Update Testando.c

--- Testando.c
+++ Testando.c
@@ -107,7 +107,7 @@ int main (int argc, char* args[])
                 
             case SDL_KEYDOWN:            
             SDL_SetRenderDrawColor(ren, 255,255,255,0);
-            SDL_RenderClear(ren);
+            //SDL_RenderClear(ren);
             SDL_SetRenderDrawColor(ren, 0,0,255,0);
             SDL_RenderFillRect(ren, &r);       
             SDL_RenderPresent(ren); 
