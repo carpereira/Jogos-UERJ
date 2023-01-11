10dee0e2916799b1ba473b38d51f97881957e73f
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-10 23:09:04 -0300

    Update Exercício1.4.1.c

--- Exercício1.4.1.c
+++ Exercício1.4.1.c
@@ -23,7 +23,7 @@ int main (int argc, char* args[])
     SDL_RenderPresent(ren);
     
     while (SDL_WaitEvent(&evt)){
-    //while (1) {                      
+        while (1) {                      
         //SDL_WaitEvent(&evt);        
         if (evt.type == SDL_MOUSEBUTTONDOWN){                        
                 r.x=evt.motion.x, r.y=evt.motion.y;            
@@ -122,7 +122,8 @@ int main (int argc, char* args[])
                 SDL_RenderFillRect(ren, &r);
                 //j++;
                 SDL_RenderPresent(ren);                
-        }  
+        }
+        }   
         //}
         /*else{
             SDL_SetRenderDrawColor(ren, 255,255,255,0);
