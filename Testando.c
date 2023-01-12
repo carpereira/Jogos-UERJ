0c91faea450f9ae8f3b1a3df5ed87e2490990134
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-11 21:13:14 -0300

    Update Testando.c

--- Testando.c
+++ Testando.c
@@ -82,14 +82,14 @@ int main (int argc, char* args[])
                         SDL_RenderFillRect(ren, &r);                
                         SDL_RenderPresent(ren);
                         break;
-        }                        
+                    }                        
                     if (r.x=evt.motion.x, r.y=evt.motion.y){            
                         SDL_SetRenderDrawColor(ren, 255,255,255,0);                       
                         SDL_SetRenderDrawColor(ren, 255,165,0,0);           
                         SDL_RenderFillRect(ren, &r);                
                         SDL_RenderPresent(ren);
                         break;
-        }                     
+                    }                     
                     if (r.x=evt.motion.x, r.y=evt.motion.y){            
                         SDL_SetRenderDrawColor(ren, 255,255,255,0);            
                         SDL_SetRenderDrawColor(ren, 255,105,180,0);            
