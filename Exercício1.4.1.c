39a6fe3cca8ff05270f5e6bb1c800db06773804d
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-14 18:57:34 -0300

    Update Exercício1.4.1.c

--- Exercício1.4.1.c
+++ Exercício1.4.1.c
@@ -119,7 +119,7 @@ int main (int argc, char* args[])
         }
                   
         if (evt.type == SDL_KEYDOWN) {             
-            //SDL_SetRenderDrawColor(ren, 255,255,255,0);
+            SDL_SetRenderDrawColor(ren, 255,255,255,0);
             SDL_RenderClear(ren);
             SDL_SetRenderDrawColor(ren, 0,0,255,0);
             SDL_RenderFillRect(ren, &r);       
