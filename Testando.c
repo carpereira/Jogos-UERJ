61cb9d461b44b4a5b4d30f251ebe66a91e033338
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-19 18:16:42 -0300

    Update Testando.c

--- Testando.c
+++ Testando.c
@@ -86,6 +86,7 @@ int main (int argc, char* args[])
                         r.x += 5;
                         break;} 
             }
+            }
         }
                     
          if (evt.type == SDL_MOUSEBUTTONDOWN){
