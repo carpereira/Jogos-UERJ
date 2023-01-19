e5e0e95fd2b17a10aa54417d6596547f797319fc
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-19 09:24:00 -0300

    Update Testando.c

--- Testando.c
+++ Testando.c
@@ -68,6 +68,7 @@ int main (int argc, char* args[])
             SDL_SetRenderDrawColor(ren, 0,0,255,0);
             SDL_RenderFillRect(ren, &r);       
             SDL_RenderPresent(ren);*/ 
+             if(a<10){
              for(a=0;a<10;a++){
                  switch(a){                    
                     
@@ -160,6 +161,7 @@ int main (int argc, char* args[])
                         SDL_RenderPresent(ren);
                         break;}           
        } 
+             }
         }
         }
     }
