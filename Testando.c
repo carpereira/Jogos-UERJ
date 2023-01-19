780a7038203f83f3eba2f2c5201d361b689ea15c
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-19 11:45:54 -0300

    Update Testando.c

--- Testando.c
+++ Testando.c
@@ -29,11 +29,11 @@ int main (int argc, char* args[])
     
     while(1){
         
-        /*SDL_SetRenderDrawColor(ren, 255,255,255,0);
+        SDL_SetRenderDrawColor(ren, 255,255,255,0);
         SDL_RenderClear(ren);
         SDL_SetRenderDrawColor(ren, 0,0,255,0);
         SDL_RenderFillRect(ren, &r);       
-        SDL_RenderPresent(ren);*/
+        SDL_RenderPresent(ren);
         
         SDL_WaitEvent(&evt);
         
@@ -42,11 +42,11 @@ int main (int argc, char* args[])
                     break;}
         
         if(evt.type == SDL_KEYDOWN){ 
-            SDL_SetRenderDrawColor(ren, 255,255,255,0);
-            //SDL_RenderClear(ren);
+            /*SDL_SetRenderDrawColor(ren, 255,255,255,0);
+            SDL_RenderClear(ren);
             SDL_SetRenderDrawColor(ren, 0,0,255,0);
             SDL_RenderFillRect(ren, &r);       
-            SDL_RenderPresent(ren);
+            SDL_RenderPresent(ren);*/
             switch (evt.key.keysym.sym){                    
                     case SDLK_UP:
                     if (r.y>0){
