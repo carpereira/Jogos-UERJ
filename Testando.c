e7cf52ffb1df5310e435ab05dd8e3c8692d86fbf
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-19 11:17:35 -0300

    Update Testando.c

--- Testando.c
+++ Testando.c
@@ -36,12 +36,17 @@ int main (int argc, char* args[])
                     break;}
         
         if(evt.type == SDL_KEYDOWN){ 
-            SDL_SetRenderDrawColor(ren, 255,255,255,0);
+            /*SDL_SetRenderDrawColor(ren, 255,255,255,0);
             SDL_RenderClear(ren);
-            //SDL_SetRenderDrawColor(ren, 0,0,255,0);
+            SDL_SetRenderDrawColor(ren, 0,0,255,0);
+            SDL_RenderFillRect(ren, &r);       
+            SDL_RenderPresent(ren);*/
+            switch (evt.key.keysym.sym){
+                    SDL_SetRenderDrawColor(ren, 255,255,255,0);
+            SDL_RenderClear(ren);
+            SDL_SetRenderDrawColor(ren, 0,0,255,0);
             SDL_RenderFillRect(ren, &r);       
             SDL_RenderPresent(ren);
-            switch (evt.key.keysym.sym){
                     case SDLK_UP:
                     if (r.y>0){
                         r.y -= 5;
