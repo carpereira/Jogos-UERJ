cbf7b871fbb148f962e42262d7714044374ef17c
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-19 13:28:23 -0300

    Update Testando.c

--- Testando.c
+++ Testando.c
@@ -65,7 +65,7 @@ int main (int argc, char* args[])
                       switch (evt.key.keysym.sym){ 
                               //SDL_SetRenderDrawColor(ren, 255,255,255,0);
             //SDL_RenderClear(ren);
-            SDL_SetRenderDrawColor(ren, 0,0,255,0);
+            //SDL_SetRenderDrawColor(ren, 0,0,255,0);
             SDL_RenderFillRect(ren, &r);       
             SDL_RenderPresent(ren);
                     case SDLK_UP:
