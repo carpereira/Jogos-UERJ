a0926a8da4e0ee14d82cd476cd89e03294297dac
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-19 01:07:28 -0300

    Update Testando.c

--- Testando.c
+++ Testando.c
@@ -63,11 +63,11 @@ int main (int argc, char* args[])
         }
                     
          if (evt.type == SDL_MOUSEBUTTONDOWN){
-            SDL_SetRenderDrawColor(ren, 255,255,255,0);
+            /*SDL_SetRenderDrawColor(ren, 255,255,255,0);
             SDL_RenderClear(ren);
             SDL_SetRenderDrawColor(ren, 0,0,255,0);
             SDL_RenderFillRect(ren, &r);       
-            SDL_RenderPresent(ren); 
+            SDL_RenderPresent(ren);*/ 
              for(a=0;a<10;a++){
                  switch(a){                    
                     //if (r.x=evt.motion.x, r.y=evt.motion.y){
