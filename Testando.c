ae4d53e3c943ad8fe005d126f6362ddb50f88153
carpereira <59072054+carpereira@users.noreply.github.com>
2023-01-19 11:38:56 -0300

    Update Testando.c

--- Testando.c
+++ Testando.c
@@ -21,19 +21,19 @@ int main (int argc, char* args[])
     SDL_Rect s = {100,100,10,10};
     SDL_Event evt;    
    
-    /*SDL_SetRenderDrawColor(ren, 255,255,255,0);
+    SDL_SetRenderDrawColor(ren, 255,255,255,0);
     SDL_RenderClear(ren);
     SDL_SetRenderDrawColor(ren, 0,0,255,0);
     SDL_RenderFillRect(ren, &r);       
-    SDL_RenderPresent(ren);*/
+    SDL_RenderPresent(ren);
     
     while(1){
         
-        SDL_SetRenderDrawColor(ren, 255,255,255,0);
+        /*SDL_SetRenderDrawColor(ren, 255,255,255,0);
         SDL_RenderClear(ren);
         SDL_SetRenderDrawColor(ren, 0,0,255,0);
         SDL_RenderFillRect(ren, &r);       
-        SDL_RenderPresent(ren);
+        SDL_RenderPresent(ren);*/
         
         SDL_WaitEvent(&evt);
         
